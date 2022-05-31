9b4fcc75f38dec28e2acdb178834b6bb9797cb32
EmadDeve20 <emaddeve20@gmail.com>
2022-05-31 21:44:56 +0430

    set the number of linees in the log file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use chrono::offset::Local;
+use chrono::Timelike;
 use std::fs;
 use std::io;
 use std::io::Write;
 use std::process;
-use chrono::Timelike;
-use chrono::offset::Local;
 
 fn main() {
     let mut file = fs::File::create("key.log").unwrap_or_else(|err| {
         eprint!("can not open the file: {}", err);
         process::exit(1);
     });
+
+    let mut number_of_line = 1;
 
     loop {
         let mut keylogger = String::new();
@@ -18,16 +20,17 @@ fn main() {
             process::exit(1);
         });
 
-        let now =  Local::now();
+        let now = Local::now();
         let now = format!("\t\t\t{}:{}:{}\n", now.hour(), now.minute(), now.second());
-        
         keylogger.pop();
         keylogger.push_str(&now);
 
         // println!("{:#?}", now);
 
-        write!(file, "{}", keylogger).unwrap_or_else(|_| {
+        write!(file, "{} -\t {}", number_of_line, keylogger).unwrap_or_else(|_| {
             process::exit(1);
         });
+
+        number_of_line += 1;
     }
 }
