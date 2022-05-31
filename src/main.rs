eff63c559ee9e384529a7d07394084889853cbae
EmadDeve20 <emaddeve20@gmail.com>
2022-05-31 21:16:41 +0430

    Write a keylogger in the simplest possible way

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::process;
+use std::io::Write;
+use std::fs;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut file = fs::File::create("key.log").unwrap_or_else(|err| {
+        eprint!("can not open the file: {}", err);
+        process::exit(1);
+    });
+
+
+    loop {
+        let mut keylogger = String::new();
+
+        io::stdin().read_line(&mut keylogger).unwrap_or_else(|_| {
+            process::exit(1);
+        });
+        
+        println!("{}", keylogger);
+        write!(file, "{}", keylogger).unwrap_or_else(|_| {
+            process::exit(1);
+        });
+    }
 }
