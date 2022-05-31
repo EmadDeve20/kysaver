46d90339209f16838e25c2aab0d254d9f1e50103
EmadDeve20 <emaddeve20@gmail.com>
2022-05-31 21:37:01 +0430

    Save time in log file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use std::process;
-use std::io::Write;
 use std::fs;
 use std::io;
+use std::io::Write;
+use std::process;
+use chrono::Timelike;
+use chrono::offset::Local;
 
 fn main() {
-    
     let mut file = fs::File::create("key.log").unwrap_or_else(|err| {
         eprint!("can not open the file: {}", err);
         process::exit(1);
     });
-
 
     loop {
         let mut keylogger = String::new();
@@ -17,8 +17,15 @@ fn main() {
         io::stdin().read_line(&mut keylogger).unwrap_or_else(|_| {
             process::exit(1);
         });
+
+        let now =  Local::now();
+        let now = format!("\t\t\t{}:{}:{}\n", now.hour(), now.minute(), now.second());
         
-        println!("{}", keylogger);
+        keylogger.pop();
+        keylogger.push_str(&now);
+
+        // println!("{:#?}", now);
+
         write!(file, "{}", keylogger).unwrap_or_else(|_| {
             process::exit(1);
         });
